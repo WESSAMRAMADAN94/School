afd82f3d41b1ec77c517b50ebc500b1b9940dce3
WESSAMRAMADAN94 <62641655+WESSAMRAMADAN94@users.noreply.github.com>
2020-03-31 06:19:30 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+ <form>
+<input type="text" name="username" placeholder="Your Username"><br>
+<input type="password" name="password" placeholder="Your Password">
+<button name="login"></button>
+ </form>   
+</body>
+</html>
